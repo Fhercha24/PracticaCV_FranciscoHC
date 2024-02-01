163b2ed902b515e60e94c17cd3c7b998e3dffdb0
francisco <franciscoherrerachacon@gmail.com>
2024-02-01 10:07:45 +0100

    Agregar archivos de proyecto.

---
+++ PracticaCV_FranciscoHC/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PracticaCV_FranciscoHC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
